476ba8bec9b876b79889e8ea5183ac5eafaa38e3
suzii21 <136336201+suzii21@users.noreply.github.com>
2023-07-31 22:18:29 +0530

    Add files via upload

---
+++ PRACTICAL/arithematic operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    int ans;
+    printf("Enter the value:\nA: ");
+    scanf("%d", &a);
+    printf("B: ");
+    scanf("%d", &b);
+    ans = a + b;
+    printf("\nAddition of a and b is: %d", ans);
+    printf("\nSubtraction: %d", a-b);
+    printf("\nMultiplication of a and b is: %d", a*b);
+    printf("\nDivision of a and b is: %f",(float)a/ (float)b);
+    printf("\nModulo: %d", a%b);
+    return 0;
+}
---
+++ PRACTICAL/practical.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int array[10]={1,4,2,7,8,5,9,15,21,24};
+    printf("\nvalue of array[8]: %d", array[5]);
+    return 0;
+}
