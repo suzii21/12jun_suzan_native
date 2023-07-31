e48ca315d9f0d085de04abc4c9a9d1053957ff55
suzii21 <136336201+suzii21@users.noreply.github.com>
2023-07-31 22:27:39 +0530

    Add files via upload

---
+++ PRACTICAL/pointer.c
@@ -0,0 +1,17 @@
+// Pointer use to store the address of te other variable
+//WAP to store the address of the variable using pointer
+#include<stdio.h>
+int main()
+{
+    int a = 85;
+    int *ptr;// create a pointer variable
+    ptr = &a;
+    printf("Address of the a is:%d", ptr);
+    // ptr++;
+    // printf("\nAddress of the a after increment is:%d ", ptr);
+    printf("\nValue of the a is: %d", *ptr);
+    *ptr = 15;
+    printf("\nValue of the a is: %d", a);
+    ++*ptr;
+    printf("\nAfter increment value of the a is:%d", a);
+    return 0;
