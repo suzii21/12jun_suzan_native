109f068a86ab7cf096a9ae63061866ca39f70f15
suzii21 <136336201+suzii21@users.noreply.github.com>
2023-08-22 22:09:06 +0530

    updated on 22 Aug 23

--- Assignment/module-3/QUE-028.C
+++ Assignment/module-3/QUE-028.C
@@ -14,4 +14,43 @@ UNION:
 -Only one member can be accessed at a time.
 -Only the first member of a union can be initialized.
 -It allows only one variable member at once.
--It is used to store one data at a time from different data types. 
+-It is used to store one data at a time from different data types.
+
+
+Structure syntax:
+
+#include<stdio.h>
+struct stdata 
+{
+    int no;
+    char name[10];
+}obj;
+int main() 
+{
+    printf("\nenter your no: "); 
+    scanf("%d", &obj.no);
+    printf("\nenter your name: ");
+    scanf("%s", &obj.name);
+    printf("\n%s your no is : %d", obj.name, obj.no);
+    return 0;
+}
+
+
+
+Union syntax:
+
+#include<stdio.h>
+union stdata{
+    int no;
+    char name[10];
+}obj;
+int main() {
+    printf("Enter your roll no: ");
+    scanf("%d", &obj.no);
+    printf("Enter your name: ");
+    scanf("%s", &obj.name);
+    printf("\n%s your roll no is: %d", obj.name, obj.no);
+    return 0;
+}
+
+
